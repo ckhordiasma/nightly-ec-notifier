914f52268d0e91ac354f9b7ba31a7e3892c728db
Christopher Kodama <ckodama@redhat.com>
2025-01-27 07:00:31 -0500

    add parameterization to monitor-snapshot
    also set its output to a file,
    
    and add kubectl wait for the snapshot just in case

--- monitor-snapshot.sh
+++ monitor-snapshot.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+set -e 
 
-# kubectl get snapshot
+# USAGE:
+# ./monitor_snapshot.sh SNAPSHOT_NAME INTEGRATION_TEST_NAME OUTPUT_FILE_NAME
 
-# TODO parameterize the input snapshot_name and integration_test
+snapshot=$1
+integration_test=$2
+output_file=$3
 
-epoch=$(date +%s)
-snapshot_name="snapshot-sample-$epoch"
+# epoch=$(date +%s)
+# 
+# snapshot="snapshot-sample-$epoch"
+# integration_test=konflux-sandbox-enterprise-contract
+# output_file=./ec-logs.txt
+# 
+# kubectl get snapshot konflux-sandbox-x8wck -o json | jq --arg name "$snapshot" '{apiVersion, kind, spec, metadata: {name: $name, namespace: .metadata.namespace, labels: {"test.appstudio.openshift.io/type": "override"} }}' | kubectl apply -f -
 
+kubectl wait --for create snapshot "$snapshot"
+kubectl label snapshot "$snapshot" "test.appstudio.openshift.io/run=$integration_test"
 
-kubectl get snapshot konflux-sandbox-x8wck -o json | jq --arg name "$snapshot_name" '{apiVersion, kind, spec, metadata: {name: $name, namespace: .metadata.namespace, labels: {"test.appstudio.openshift.io/type": "override"} }}' | kubectl apply -f -
-
-integration_test=konflux-sandbox-enterprise-contract
-
-kubectl label snapshot "$snapshot_name" "test.appstudio.openshift.io/run=$integration_test"
-
-pipelinerun=$(kubectl get pr -l "appstudio.openshift.io/snapshot=$snapshot_name,test.appstudio.openshift.io/scenario=$integration_test" --no-headers | awk '{print $1}')
+pipelinerun=$(kubectl get pr -l "appstudio.openshift.io/snapshot=$snapshot,test.appstudio.openshift.io/scenario=$integration_test" --no-headers | awk '{print $1}')
 
 pod_name="${pipelinerun}-verify-pod"
 
@@ -24,11 +29,7 @@ echo "waiting for $pod_name to finish"
 kubectl wait --for='jsonpath={.status.conditions[?(@.reason=="PodCompleted")].status}=True' pod "$pod_name"
 
 
-logs=$(kubectl logs "$pod_name" step-report-json)
+kubectl logs "$pod_name" step-report-json > $output_file
 
-echo $logs
 
-# TODO parse logs
-
-# TODO send to slack
 
